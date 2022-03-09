316332e36634a7b8f09a456d618381bddd2aafbb
Robert Jr. Maestro <ilovenine097@yahoo.com>
2022-03-10 01:16:47 +0800

    a

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif']
+      },
+      colors: {
+        primary: '#11823b',
+        bglight: '#FBFFFC'
+      }
+    }
+  },
+  plugins: [],
+}
